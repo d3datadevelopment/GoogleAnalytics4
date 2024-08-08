48a598da515b77b552fe060e57296d5e4bd64345
MarkusGaertner <mg@shopmodule.com>
2024-08-08 15:03:48 +0200

    render() ergänzen

--- Application/Component/Widget/d3GtmStartWidget.php
+++ Application/Component/Widget/d3GtmStartWidget.php
@@ -6,5 +6,10 @@ namespace D3\GoogleAnalytics4\Application\Component\Widget;
 
 class d3GtmStartWidget extends \OxidEsales\Eshop\Application\Component\Widget\WidgetController
 {
+    public function render()
+    {
+        parent::render();
 
+        return '';
+    }
 }
